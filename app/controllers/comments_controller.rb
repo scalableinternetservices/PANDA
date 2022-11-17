52208398d1bbd82fe5c1255b6072392574ee95c1
zephyrz73 <xzhou1551@outlook.com>
2022-11-16 22:05:16 -0800

    delete verify authentication token

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-    # GET /comments or /comments.json
-    skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
+  #   # GET /comments or /comments.json
+  #   skip_before_action :verify_authenticity_token
     def index
       @comments = Comment.all
     end
