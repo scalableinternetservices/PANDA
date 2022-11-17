260f7f1df75d1a29e062d2ed778ed52e009a4780
zephyrz73 <xzhou1551@outlook.com>
2022-11-16 21:28:11 -0800

    change post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
-    # protect_from_forgery with: :null_session
+    protect_from_forgery with: :null_session
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
